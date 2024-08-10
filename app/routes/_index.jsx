792b1d92253827b55559d57de42611bfee76f4a6
Rowena Foo <rwnfoo@gmail.com>
2024-08-10 14:14:45 -0500

    display collections on home page

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,4 +1,4 @@
-import {useLoaderData} from '@remix-run/react';
+import {useLoaderData, Link} from '@remix-run/react';
 
 export function meta() {
     return [
@@ -13,11 +13,21 @@ export function meta() {
   
   export default function Index() {
     const {collections} = useLoaderData();
-    console.log(collections);
     return (
-      <div>
-        <h1>Hello from the home page!</h1>
-      </div>
+      <section className="w-full gap-4">
+        <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+          Collections
+        </h2>
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
+          {collections.nodes.map((collection) => {
+            return (
+              <Link to={`/collections/${collection.handle}`} key={collection.id}>
+                {collection.title}
+              </Link>
+            );
+          })}
+        </div>
+      </section>
     );
   }
 
