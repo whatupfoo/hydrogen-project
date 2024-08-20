d8e0dfda0f731d584861a6e6494a285131ab5c8a
Rowena Foo <rwnfoo@gmail.com>
2024-08-20 10:37:53 -0500

    query for display title, description etc

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,7 +1,75 @@
-export default function ProductHandle() {
-    return (
-      <div className="product-wrapper">
-        <h2>Product content will go here</h2>
-      </div>
-    );
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+
+export async function loader({params, context}) {
+  const {handle} = params;
+  const {product} = await context.storefront.query(PRODUCT_QUERY, {
+    variables: {
+      handle
+    }
+  }); 
+  if (!product?.id) {
+    throw new Response(null, {status: 404});
   }
+
+  return json({
+    handle,
+    product,
+  });
+}
+export default function ProductHandle() {
+  const {product} = useLoaderData();
+
+  return (
+    <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
+      <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
+        <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-hidden md:grid-cols-2 md:w-full lg:col-span-2">
+          <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
+            <h2>TODO Product Image</h2>
+          </div>
+        </div>
+        <div className="md:sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-2 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
+          <div className="grid gap-2">
+            <h1 className="text-4xl font-bold leading-10 whitespace-normal">
+              {product.title}
+            </h1>
+            <span className="max-w-prose whitespace-pre-wrap inherit text-copy opacity-50 font-medium">
+              {product.vendor}
+            </span>
+          </div>
+          <h3>TODO Product Options</h3>
+          <div
+            className="prose border-t border-gray-200 pt-6 text-black text-md"
+            dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+          ></div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+const PRODUCT_QUERY = `#graphql
+  query product($handle: String!) {
+    product(handle: $handle) {
+      id
+      title
+      handle
+      vendor
+      description
+      descriptionHtml
+      featuredImage{
+        id
+        url
+        altText
+        width
+        height
+      }
+      options {
+        name,
+        optionValues {
+          name
+        }
+      }
+    }
+  }
+`;
