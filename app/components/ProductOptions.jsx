a1efc1c265250e769f326661c5da1787b281e58e
Rowena Foo <rwnfoo@gmail.com>
2024-08-23 10:56:41 -0500

    selected variant

--- app/components/ProductOptions.jsx
+++ app/components/ProductOptions.jsx
@@ -1,31 +1,15 @@
 import { Link, useLocation, useSearchParams, useNavigation } from '@remix-run/react';
 
-export default function ProductOptions({options, selectedVariant}) {
-  const {pathname, search} = useLocation();
-  const [currentSearchParams] = useSearchParams();
+export default function ProductOptions({ options, selectedVariant }) {
   const navigation = useNavigation();
-
-  const paramsWithDefaults = (() => {
-    const defaultParams = new URLSearchParams(currentSearchParams);
-
-    if (!selectedVariant) {
-      return defaultParams;
-    }
-
-    for (const {name, value} of selectedVariant.selectedOptions) {
-      if (!currentSearchParams.has(name)) {
-        defaultParams.set(name, value);
-      }
-    }
-
-    return defaultParams;
-  })();
+  const { pathname } = useLocation();
+  const [currentSearchParams] = useSearchParams();
 
   // Update the in-flight request data from the 'navigation' (if available)
-  // to create an optimistic UI that selects a link before the request is completed
+  // to create an optimistic UI that selects a link before the request completes
   const searchParams = navigation.location
     ? new URLSearchParams(navigation.location.search)
-    : paramsWithDefaults;
+    : currentSearchParams;
 
   return (
     <div className="grid gap-4 mb-6">
